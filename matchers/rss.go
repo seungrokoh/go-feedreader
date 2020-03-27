f07680de4670e8f88db2f49ae713abbbdecab999
seungrokoh <devdavidoh@gmail.com>
2020-03-27 20:53:26 +0900

    add : rss matcher 구현

---
+++ matchers/rss.go
@@ -0,0 +1,126 @@
+package matchers
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"github.com/seungrokoh/go-feedreader/search"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+type (
+	// item 구조체는 RSS 문서 내의 item 태그에
+	// 정의된 필드들에 대응하는 필드들을 선언한다.
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		PubDate     string   `xml:"pubDate"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+		Link        string   `xml:"link"`
+		GUID        string   `xml:"guid"`
+		GeoRssPoint string   `xml:"georss:point"`
+	}
+
+	// image 구조체는 RSS 문서 내의 image 태그에
+	// 정의된 필드들에 대응하는 필드들을 선언한다.
+	image struct {
+		XMLName xml.Name `xml:"image"`
+		URL     string   `xml:"url"`
+		Title   string   `xml:"title"`
+		Link    string   `xml:"link"`
+	}
+
+	// channel 구조체는 RSS 문서 내의 channel 태그에
+	// 정의된 필드들에 대응하는 필드들을 선언한다.
+	channel struct {
+		XMLName        xml.Name `xml:"channel"`
+		Title          string   `xml:"title"`
+		Description    string   `xml:"description"`
+		Link           string   `xml:"link"`
+		PubDate        string   `xml:"pubDate"`
+		LastBuildDate  string   `xml:"lastBuildDate"`
+		TTL            string   `xml:"ttl"`
+		Language       string   `xml:"language"`
+		ManagingEditor string   `xml:"managingEditor"`
+		WebMaster      string   `xml:"webMaster"`
+		Image          image    `xml:"image"`
+		Item           []item   `xml:"item"`
+	}
+
+	// rssDocument 구조체는 RSS 문서에 정의된 필드들에 대응하는 필드들을 정의한다.
+	rssDocument struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
+)
+
+type rssMatcher struct{}
+
+func init() {
+	var matcher rssMatcher
+	search.Register("rss", matcher)
+}
+
+func (m rssMatcher) Search(feed *search.Feed, searchTerm string) ([]*search.Result, error) {
+	var results []*search.Result
+
+	log.Printf("피드 종류[%s] 사이트[%s] 주소[%s]에서 검색을 수행합니다.\n", feed.Type, feed.Name, feed.URI)
+
+	document, err := m.retrieve(feed)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, channelItem := range document.Channel.Item {
+		matched, err := regexp.MatchString(searchTerm, channelItem.Title)
+		if err != nil {
+			return nil, err
+		}
+
+		// 검색어가 발견되면 결과에 저장
+		if matched {
+			results = append(results, &search.Result{
+				Field:   "Title",
+				Content: channelItem.Title,
+			})
+		}
+
+		// 상세 내용에서 검색어를 검색
+		matched, err = regexp.MatchString(searchTerm, channelItem.Description)
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			results = append(results, &search.Result{
+				Field:   "Description",
+				Content: channelItem.Description,
+			})
+		}
+	}
+
+	return results, nil
+}
+
+func (m rssMatcher) retrieve(feed *search.Feed) (*rssDocument, error) {
+	if feed.URI == "" {
+		return nil, errors.New("검색할 RSS 피드가 정의되지 않았습니다")
+	}
+
+	resp, err := http.Get(feed.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP 응답 오류: %d\n", resp.StatusCode)
+	}
+
+	var document rssDocument
+	err = xml.NewDecoder(resp.Body).Decode(&document)
+	return &document, err
+}
