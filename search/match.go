bfccb7cacdf416c47a790ed934bff9624ce70f31
seungrokoh <devdavidoh@gmail.com>
2020-03-27 20:34:38 +0900

    add : match.go 추가

--- search/match.go
+++ search/match.go
@@ -1,1 +1,32 @@
 package search
+
+import "log"
+
+type Result struct {
+	Field   string
+	Content string
+}
+
+type Matcher interface {
+	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+func Match(matcher Matcher, feed *Feed, searchTerm string, results chan<- *Result) {
+	// 지정된 검색기를 이용해 검색을 수행
+	searchResults, err := matcher.Search(feed, searchTerm)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// 검색 결과를 채널에 기록
+	for _, result := range searchResults {
+		results <- result
+	}
+}
+
+// 개별 고루틴이 전달한 검색 결과를 콘솔에 출력
+func Display(results chan *Result) {
+	for result := range results {
+		log.Printf("%s:\n%s\n\n", result.Field, result.Content)
+	}
+}
