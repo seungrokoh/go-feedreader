0b203f88e3ea65dc6cfa35a8628f7140d83292de
seungrokoh <devdavidoh@gmail.com>
2020-03-30 11:24:07 +0900

    add : flag 이용해서 command input 사용하기

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	_ "github.com/seungrokoh/go-feedreader/matchers"
 	"github.com/seungrokoh/go-feedreader/search"
 	"log"
 	"os"
-	"runtime"
 )
 
 func init() {
@@ -14,6 +13,11 @@ func init() {
 }
 
 func main() {
-	search.Run("코로나")
-	fmt.Println("num of goroutine: ", runtime.NumGoroutine())
+	keyword := flag.String("keyword", "", "search keyword")
+	flag.Parse()
+	search.Run(*keyword)
 }
+
+// 1. go run main.go -keyword=corona flag
+// 2. http.Get에 타임아웃 걸기 100 * time.Milliseconds
+// 3. cancel goroutine
